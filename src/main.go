5254d734e87ffb3509f6535971474dd1ca0a341b
Isaiasgomz <isaiasrobles2003@gmail.com>
2023-03-15 10:18:37 -0600

    cycles

--- src/main.go
+++ src/main.go
@@ -1,10 +1,171 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+
+func normalFunction(message string)  {
+	fmt.Println(message)
+}
+
+func manyArgs(a int, b int, c string){
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int{
+	return a*2
+}
+
+func doubeReturnValue(a int)(c , d int){
+	return a , a * 2
+}
 
 func main() {
 	fmt.Println("hello world")
 
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+		fmt.Println("this the value of the PI :",pi, "this is other value:", pi2 )
+	base := 12
+	var altura int = 16
+	var area int
+	fmt.Println(base, altura, area)
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Println(a, b, c, d)
+
+	const baseCuadrado int = 10
+	areaCuadrado := baseCuadrado* baseCuadrado
+	fmt.Println(areaCuadrado)
+
+
+	x := 10
+	y := 50
+
+	result := x + y
+	fmt.Println(result)
+
+	result = x - y
+	fmt.Println(result)
+
+	result = x * y
+	fmt.Println(result)
+
+	result = x / y
+	fmt.Println(result)
+
+	result = x % y
+	fmt.Println(result) 
+
+	 helloMessage := "hello"
+	wordMessage := "word"
+
+	fmt.Println(helloMessage, wordMessage) 
+
+
+
+	platzi := "platzi"
+	cursos := 500
+
+	fmt.Printf("%s tiene mas de %d curso\n",platzi,cursos)
+
+	message := fmt.Sprintf("%v tiene mas de %v cursos \n",platzi, cursos)
+	fmt.Println(message)
+
+
+	fmt.Printf("helloMessage:\n", helloMessage)
+	fmt.Printf("wordMessage:\n", wordMessage)
+
+	normalFunction("helloMessage")
+	manyArgs(1,2, "helloMessage") 
+
+	value := returnValue(5)
+	fmt.Println(value)
+
+	value1, value2 := doubeReturnValue(5)
+	fmt.Println(value1, value2) 
+
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	counter:= 0
+	for counter < 10 {
+		fmt.Println(counter)
+        counter++
+	}
+
+	// counterForever := 0
+	// for  {
+	// 	fmt.Println(counterForever)
+	// 	counterForever++
+	// }
+
+	valueOne := 2
+	valueTwo := 3
+    
+	if valueOne == 2 {
+		fmt.Println("valueone is equal a 2")
+	}else{
+		fmt.Println("valueone is not equal a 2")
+	}
+
+	if valueOne == 2 && valueTwo == 3{
+	fmt.Println("es verdad")
+	}
+
+	if valueOne == 0 || valueTwo == 3 {
+		fmt.Println("valueOne is 0 or valueTwo is 3")
+	}
+
+	value, errr := strconv.Atoi("45")
+	if errr!= nil {
+        log.Fatal(errr)
+	}
+	fmt.Println("vlaue:", value)
+
+	modulo := 4% 2
+	switch modulo {
+		case 0:
+            fmt.Println("modulo is par")
+        default:
+            fmt.Println("modulo isn't par")
+    
+	}
+
+	valueForSwitch := 200
+
+	switch  {
+		case valueForSwitch > 100 :
+            fmt.Println("valueForSwitch is more than 100")
+		case valueForSwitch < 0: 
+		    fmt.Println("valueForSwitch is less than 0")
+        default:
+            fmt.Println("valueForSwitch isn't number")
+	}
+
+
+	defer	fmt.Println("hello")
+	fmt.Println("word")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		if i == 3{
+			fmt.Println("this is",i)
+			continue
+		}
+		if i == 8 {
+			fmt.Println("break",i)
+            break
+		}
+	}
 
 	
 }
